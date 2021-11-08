47846be8c1adb7b31fc989cf06059f8737d59ec6
Helisson <helisson.araujo@aluno.ifsp.edu.br>
2021-11-08 01:15:42 -0300

    Criação da API em Django

---
+++ API/API/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from escola.views import AlunoViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculasAluno, ListaAlunosMatriculados
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'alunos', AlunoViewSet, basename='Alunos')
+router.register(r'cursos', CursoViewSet, basename='Cursos')
+router.register(r'matriculas', MatriculaViewSet, basename='Matriculas')
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path('alunos/<int:pk>/matriculas', ListaMatriculasAluno.as_view()),
+    path('cursos/<int:pk>/matriculas', ListaAlunosMatriculados.as_view())
+]
